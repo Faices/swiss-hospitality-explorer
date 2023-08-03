2ddd6b15331580db74cb2476b4b75ce983e8c059
datachalet <139548802+datachalet@users.noreply.github.com>
2023-08-03 12:22:21 +0200

    dynamic main col for map

--- app.py
+++ app.py
@@ -1586,6 +1586,9 @@ with expander:
         color1, color2, color3, color4, color5, color6, color7, color8, color9, color10,
         color11, color12, color13, color14, color15, color16, color17, color18, color19, color20
     ]
+
+primaryColor=color1 #for the map
+
 st.sidebar.divider() 
 st.sidebar.caption("with :heart: by Datachalet")
 
